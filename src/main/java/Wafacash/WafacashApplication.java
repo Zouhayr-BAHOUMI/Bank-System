e345e2ea72c52a2b39a37ad10481ad6d0ab56c20
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-07-02 16:15:23 +0100

    creation du projet

---
+++ src/main/java/Wafacash/WafacashApplication.java
@@ -0,0 +1,13 @@
+package Wafacash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WafacashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WafacashApplication.class, args);
+    }
+
+}
---
+++ src/test/java/Wafacash/WafacashApplicationTests.java
@@ -0,0 +1,13 @@
+package Wafacash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WafacashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

1dfdbcc076230b6684e6576f44e6c9503dda5c97
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-07-07 04:55:35 +0100

    faire controller to handle adding a carte

--- src/main/java/Wafacash/controller/CarteController.java
+++ src/main/java/Wafacash/controller/CarteController.java
@@ -1,10 +1,28 @@
 package Wafacash.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import Wafacash.model.Carte;
+import Wafacash.service.CarteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/compte")
+@RequestMapping("/carte")
 public class CarteController {
+
+    @Autowired
+    private CarteService carteService;
+
+
+    @PostMapping("/add")
+    public ResponseEntity<String> ajouterCarte(@RequestBody Carte carte){
+        try {
+            carteService.addCarte(carte);
+            return ResponseEntity.status(HttpStatus.CREATED).body("created successfully");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("not created" + e.getMessage());
+        }
+    }
 }

fd3365495517ebf334531dbef25d6734111d2d13
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-07-14 10:32:26 +0100

    impliment the jwt service

---
+++ src/main/java/Wafacash/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package Wafacash.config;
+
+public class SecurityConfig {
+}
--- src/main/java/Wafacash/repository/UserRepository.java
+++ src/main/java/Wafacash/repository/UserRepository.java
@@ -4,6 +4,8 @@ package Wafacash.repository;
 import Wafacash.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
 }
